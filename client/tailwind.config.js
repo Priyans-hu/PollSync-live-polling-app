b31fc54a9feae07329c6e10a574567c4c02f49a8
Priyanshu <gargpriyanshu2004@gmail.com>
2025-06-20 11:47:03 +0530

    updated Landing page design

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        primary: '#7765DA',
+        light: '#F2F2F2',
+        darkish: '#5767D0',
+        dark: '#373737',
+        accent: '#4F0DCE',
+        grayish: '#6E6E6E',
+      },
     },
   },
   plugins: [],
